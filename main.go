0416b2b13709039b3a0a1259bb33a111234a7c9f
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-04-17 16:07:03 +0300

    Init with the files

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email string
+	zip   int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jom",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email: "jim@jim.com",
+			zip:   1000,
+		},
+	}
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
